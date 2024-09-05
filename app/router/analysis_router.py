8653b0f6bf2a1881ae1470a38c189a824e354a1d
andrew <whdms1107@gmail.com>
2024-09-05 15:16:44 +0900

    ✨ feat: analysis_router 구성
    백엔드의 요청을 처리할 수 있도록 구성
    endpoint -
    POST
    ~/analysis
    Content-type : JSON

--- app/router/analysis_router.py
+++ app/router/analysis_router.py
@@ -1,9 +1,21 @@
-import openai
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
+# from app.model.analysis_model import analysis_model
 
 router = APIRouter()
 
+
+class Diary(BaseModel):
+    content: str
+
+
 @router.post("/analysis")
-async def analysis(request: Request):
+async def analysis_router(diary: Diary):
+    content = diary.content
+    if content is None:
+        return JSONResponse({"error": "본문이 제공되지 않습니다.", "content": content}, status_code=400)
     # 일기 내용을 받아서 사용
-    pass
+
+    # response = analysis_model() 분석 함수
+    return JSONResponse(content=content)
